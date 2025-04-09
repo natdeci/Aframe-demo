1199806a0abd4766bd4a8b6820fad668e9a0b594
Deciano <deciano21.nathan12@gmail.com>
2025-04-09 16:14:17 +0700

    first commit

--- App.js
+++ App.js
@@ -1,39 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
-import * as FileSystem from 'expo-file-system';
-import { Asset } from 'expo-asset';
 
 export default function App() {
-  const [aFrameHTML, setAFrameHTML] = useState(null);
-
-  useEffect(() => {
-    const loadHTML = async () => {
-      const asset = Asset.fromModule(require('./index.html'));
-      await asset.downloadAsync();
-
-      const html = await FileSystem.readAsStringAsync(asset.localUri);
-      setAFrameHTML(html);
-    };
-
-    loadHTML();
-  }, []);
-
-  if (!aFrameHTML) return null;
-
   return (
-    <View style={styles.container}>
-      <WebView
+    <SafeAreaView style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://truemiha.github.io/aframe-office/' }}
+        style={styles.webview}
         originWhitelist={['*']}
-        source={{ html: aFrameHTML }}
-        style={{ flex: 1 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  webview: {
     flex: 1,
   },
 });
