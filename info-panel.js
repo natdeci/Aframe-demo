abd25873ccd313d2b4c909eb1f5a9b4526012a5e
Deciano <deciano21.nathan12@gmail.com>
2025-04-22 17:58:26 +0700

    fixed and adjusted assets

--- info-panel.js
+++ info-panel.js
@@ -9,19 +9,19 @@ AFRAME.registerComponent('info-panel', {
 
     this.movieInfo = {
       karigurashiButton: {
-        title: 'The Secret World of Arrietty (2010)',
+        title: 'Dashboard Geospatial',
         imgEl: document.querySelector('#karigurashiMovieImage'),
-        description: 'Based on the 1952 novel The Borrowers by Mary Norton, an English author of children\'s books, about a family of tiny people who live secretly in the walls and floors of a typical household, borrowing items from humans to survive.'
+        description: 'Website SIGA menyediakan dashboard geospasial untuk melihat statistik seputar keluarga berdasarkan peta geografi Indonesia'
       },
       kazetachinuButton: {
-        title: 'The Wind Rises (2013)',
+        title: 'Gedung BKKBN',
         imgEl: document.querySelector('#kazetachinuMovieImage'),
-        description: 'The Wind Rises is a fictionalised biographical film of Jiro Horikoshi (1903, 1982), designer of the Mitsubishi A5M fighter aircraft and its successor, the Mitsubishi A6M Zero, used by the Empire of Japan during World War II. The film is adapted from Miyazaki\'s manga of the same name, which was in turn loosely based on both the 1937 novel The Wind Has Risen by Tatsuo Hori and the life of Jiro Horikoshi.'
+        description: 'Gedung BKKBN berada di daerah Halim Perdana Kusuma, daerah Jakarta Timur'
       },
       ponyoButton: {
-        title: 'Ponyo (2003)',
+        title: 'Satgas Stunting',
         imgEl: document.querySelector('#ponyoMovieImage'),
-        description: 'It is the eighth film Miyazaki directed for Studio Ghibli, and his tenth overall. The film tells the story of Ponyo (Nara), a goldfish who escapes from the ocean and is rescued by a five-year-old human boy, Sōsuke (Doi) after she is washed ashore while trapped in a glass jar.'
+        description: 'Tersedia juga dashboard untuk satgas stunting di mana dashboard dapat menampilkan statistik tentang stunting, risiko, dan capaian indikator PPS'
       }
     };
 
